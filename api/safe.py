a301b41a74bab842e4973fe98f318970c9ba3a20
waterymonkey3 <waterymonkey3@gmail.com>
2026-06-12 21:06:36 +0800

    dependence insert

--- api/safe.py
+++ api/safe.py
@@ -1,9 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request,Depends
 
 api_safe = APIRouter()
 
+async def commonvar(
+        num1: int = 1,
+        num2: int = 2
+):
+    return num1+num2
 
 @api_safe.get('/')
 async def safe():
 
     return {'message':'Healthy'}
+
+
+@api_safe.get('/get_test')
+async def get_safe(request: Request):
+    get_safe = request.query_params
+    print(get_safe)
+    return {'message':'get_safe'}
+
+
+
+@api_safe.post('/post_test')
+async def post_safe(request: Request, common=Depends(commonvar)):
+    try:
+        num = await commonvar()
+        print(num)
+        post_safe = await request.json()
+        print(post_safe)
+        return {'message':post_safe}
+    except Exception as e:
+        print("error:"+str(e))
+        return {"error:"+str(e)}
--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = FastAPI(title='Library API', description='the API of library manage system
 app.include_router(api_safe,prefix="/safe",tags=['API Health'])
 
 
+
+@app.middleware("http")
+async def middleware1(request, call_next):
+    print("middleware1 start")
+    response = await call_next(request)
+    print("middleware1 end")
+    return response
+
+
+
 @app.get('/')
 async def root():
     return{'message':'hello world'}
